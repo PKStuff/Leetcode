dbde203104caf8ae4a1f6c0190e4646ce340037c
pradipko <pradip.kothawale@amdocs.com>
2022-01-23 17:41:51 +0530

    remove conflict

--- Group Anagrams/index.py
+++ Group Anagrams/index.py
@@ -5,13 +5,7 @@ def groupAnagrams(strs):
         if sorted_string in element_map:
             element_map[sorted_string].append(string)
         else:
-<<<<<<< HEAD
-            element_map[ascii_count] = [string]
-
-=======
             element_map[sorted_string] = [string]
-    
->>>>>>> fe8676fcbdee8ea4d12a73f08b4d6353906eefbe
     result = []
     for value in element_map.values():
         result.append(value)
@@ -19,9 +13,6 @@ def groupAnagrams(strs):
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 strs1 = ["a"]
-<<<<<<< HEAD
 strs2 = ["cab","tin","pew","duh","may","ill","buy","bar","max","doc"]
 print(groupAnagrams(strs2))
-=======
 print(groupAnagrams(strs))
->>>>>>> fe8676fcbdee8ea4d12a73f08b4d6353906eefbe
